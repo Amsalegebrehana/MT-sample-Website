007d6a3406526591a7db0e7890851a96b74cb0e4
Amsale Gebrehana <se.amsale.gebrehana@gmail.com>
2021-11-07 02:18:27 +0300

    basics

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React , {Component} from "react";
+import Navbar from './component/layout/Navbar';
+import './App.css';
+import Footer from "./component/layout/Footer";
+import Landing from "./component/layout/Landing";
+
+class App extends Component{
+    render(){
+        return(
+            <div className="App">
+                <Navbar/>
+                <Landing/>
+                <Footer/>
+            </div>
+        );
+    }
+}
+export default App;
+// ReactDOM.render(<App/>, document.getElementById('root'))
